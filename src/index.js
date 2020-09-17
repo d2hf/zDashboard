503dc2bb52f3474b157fbe82ffbf79c849a60008
Daniel L Ferreira <dl.ferreira179@gmail.com>
2020-09-16 21:56:38 -0300

    Refactor report creation
    Reports were created by directly connecting to DynamoDB. They are now created by calling an API endpoint.
    
    The API is accessed with the help of the API library from amplify.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import AWS from 'aws-sdk';
 import Auth from '@aws-amplify/auth';
-import Api from "@aws-amplify/api";
+import API from "@aws-amplify/api";
 
 Auth.configure({
     region: 'us-east-1',
@@ -9,7 +9,7 @@ Auth.configure({
     identityPoolId: 'us-east-1:716e44bc-2e9a-4ff9-afd9-a6ecdfb2d21a',
 });
 
-Api.configure({
+API.configure({
     endpoints: [
         {
             name: "ReportsApi",
@@ -32,91 +32,50 @@ async function signOut() {
 }
 
 function processForm() {
-    // retrieves data from forms
-    var totalSold = document.getElementById("salesTotal").value;
-    var totalBilled = document.getElementById("billedTotal").value;
-    var weightBilled = document.getElementById("weightBilled").value;
-    var inputJuliana = document.getElementById("inputJuliana").value;
-    var inputAnderson = document.getElementById("inputAnderson").value;
-    var inputBoaIdeia = document.getElementById("inputBoaIdeia").value;
-    var inputFerronato = document.getElementById("inputFerronato").value;
-    var inputMaxel = document.getElementById("inputMaxel").value;
-    var inputSagrima = document.getElementById("inputSagrima").value;
+    const formData = getReportFormData();
+    const timestamp = getTimestamp();
 
-
-    // adds data to dynamoDB
-    putInDataBase(totalSold, totalBilled, weightBilled, inputJuliana, inputBianfer, inputAnderson,
-        inputBoaIdeia, inputFerronato, inputMaxel, inputSagrima);
+    createReport(formData, timestamp)
+        .then(() => {
+            console.log("Yay");
+        })
+        .catch(() => {
+            console.error("Boo");
+        });
 }
 
-async function putInDataBase(totalSold, totalBilled, weightBilled, inputJuliana, inputBianfer,
-                             inputAnderson, inputBoaIdeia, inputFerronato, inputMaxel, inputSagrima) {
-    AWS.config.update({region: "us-east-1"});
+const getReportFormData = () => {
+    return {
+        totalSold: document.getElementById("salesTotal").value,
+        totalBilled: document.getElementById("billedTotal").value,
+        weightBilled: document.getElementById("weightBilled").value,
+        inputJuliana: document.getElementById("inputJuliana").value,
+        inputAnderson: document.getElementById("inputAnderson").value,
+        inputBoaIdeia: document.getElementById("inputBoaIdeia").value,
+        inputFerronato: document.getElementById("inputFerronato").value,
+        inputMaxel: document.getElementById("inputMaxel").value,
+        inputSagrima: document.getElementById("inputSagrima").value
+    };
+};
 
-    try {
-        const user = await Auth.currentCredentials();
-        var credentials = new AWS.Credentials({
-            accessKeyId: user.accessKeyId,
-            secretAccessKey: user.secretAccessKey,
-            sessionToken: user.sessionToken
-        });
-    } catch (err) {
-        console.error('Unable to retrieve credentials.');
-        console.error(err);
+const getTimestamp = () => {
+    const date = new Date();
+    return `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
+}
+
+async function createReport(data, createdAt) {
+    const apiName = "ReportsApi";
+    const path = "";
+    const options = {
+        body: {
+            createdAt,
+            ...data
+        }
     }
 
-    var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10', credentials});
-
-    const date = new Date();
-    const timestamp = `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
-
-    var params = {
-        Item: {
-            "idRelatorio": {
-                S: timestamp
-            },
-            "totalSold": {
-                S: totalSold.toString()
-            },
-            "totalBilled": {
-                S: totalBilled.toString()
-            },
-            "weightBilled": {
-                S: weightBilled.toString()
-            },
-            "createdAt": {
-                S: timestamp
-            },
-            "inputJuliana": {
-                S: inputJuliana.toString()
-            },
-            "inputBianfer": {
-                S: inputBianfer.toString()
-            },
-            "inputAnderson": {
-                S: inputAnderson.toString()
-            },
-            "inputBoaIdeia": {
-                S: inputBoaIdeia.toString()
-            },
-            "inputFerronato": {
-                S: inputFerronato.toString()
-            },
-            "inputMaxel": {
-                S: inputMaxel.toString()
-            },
-            "inputSagrima": {
-                S: inputMaxel.toString()
-            },
-        },
-        ReturnConsumedCapacity: "TOTAL",
-        TableName: "zdashboard"
-    };
-
-    dynamodb.putItem(params, function(err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else console.log(data);           // successful response
-    });
+    await API.post(apiName, path, options)
+        .then(response => console.log(response))
+        .catch(error => console.log(error));
 }
 
 function redirectLogin (){
